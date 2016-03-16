5076e37613151ec4e2098684ac34c7fd7b2357f4
vihellor <vihellor@gmail.com>
2016-03-15 22:45:51 -0600

    Update better_spoken_numbers.py

--- better_spoken_numbers.py
+++ better_spoken_numbers.py
@@ -1,61 +1,82 @@
 #!/usr/bin/python
 from math import floor
 
-num2words = {0: 'Zero ', 1: 'One ', 2: 'Two ', 3: 'Three ', 4: 'Four ', 5: 'Five ',
-             6: 'Six ', 7: 'Seven ', 8: 'Eight ', 9: 'Nine ', 10: 'Ten ',
-            11: 'Eleven ', 12: 'Twelve ', 13: 'Thirteen ', 14: 'Fourteen ',
-            15: 'Fifteen ', 16: 'Sixteen ', 17: 'Seventeen ', 18: 'Eighteen ',
-            19: 'Nineteen ', 20: 'Twenty ', 30: 'Thirty ', 40: 'Forty ',
-            50: 'Fifty ', 60: 'Sixty ', 70: 'Seventy ', 80: 'Eighty ',
-            90: 'Ninety '}
+num2words = {0: 'Cero ', 1: 'Uno ', 2: 'Dos ', 3: 'Tres ', 4: 'Cuatro ', 5: 'Cinco ',
+             6: 'Seis ', 7: 'Siete ', 8: 'Ocho ', 9: 'Nueve ', 10: 'Diez ',
+            11: 'Once ', 12: 'Doce ', 13: 'Trece ', 14: 'Catorce ',
+            15: 'Quince ', 16: ' ', 17: 'Seventeen ', 18: 'Eighteen ',
+            19: 'Nineteen ', 20: 'Veinte ', 30: 'Treinta ', 40: 'Cuarenta ',
+            50: 'Cincuenta ', 60: 'Sesenta ', 70: 'Setenta ', 80: 'Ochenta ',
+            90: 'Noventa '}
+num2cient = {1: 'ciento ', 2: 'Doscientos ', 3: 'Trescientos ', 4: 'Cuatrocientos ', 5: 'Quinientos ',
+             6: 'Seiscientos ', 7: 'Setecientos ', 8: 'Ochocientos ', 9: 'Novecientos '}
+            
+num2month = {0: 'Enero ', 1: 'Febrero ', 2: 'Marzo ', 3: 'Abril ', 4: 'Mayo ', 5: 'Junio ',
+             6: 'Julio ', 7: 'Agosto ', 8: 'Septiembre ', 9: 'Octubre ', 10: 'Noviembre ',
+            11: 'Diciembre '}
+            
+num2day = {0: 'Domingo ', 1: 'Lunes ', 2: 'Martes ', 3: 'Miercoles ', 4: 'Jueves ', 5: 'Viernes ',
+             6: 'Sabado '}
 
-date2words = {1: 'First', 2: 'Second', 3: 'Third', 4: 'Fourth', 5: 'Fifth',
-            6: 'Sixth', 7: 'Seventh', 8: 'Eighth', 9: 'Ninth', 10: 'Tenth',
-           11: 'Eleventh', 12: 'Twelfth', 13: 'Thirteenth', 14: 'Fourteenth',
-           15: 'Fifteenth', 16: 'Sixteenth', 17: 'Seventeenth', 18: 'Eighteenth',
-           19: 'Nineteenth', 20: 'Twentieth', 30: 'Thirtieth'}
 
 def n2w(n):
-  if n<=20:
+  if n<=15:
     return num2words[n]
+  elif n<20:
+    words= 'diez y '
+    words+= num2words[n%10]
   elif n<100:
     words=num2words[n-n%10]
     if n%10>0:
-      words+=num2words[n%10]
+      words+= 'y ' +num2words[n%10]
     return words
+  elif n==100:
+    word= 'cien '
   elif n<1000:
     hundreds=(n-n%100)/100
     tens=(n%100)-(n%100)%10
     singles=n-((hundreds*100)+tens)
-    words=num2words[hundreds] + ' hundred'
+    words=num2cient[hundreds]
     if tens > 0:
-      words+=' '+num2words[tens]
+      words+=num2words[tens] + 'y '
     if singles > 0:
       words+=num2words[singles]
     return words
   elif n<1000000:
     thousands=(n-n%1000)/1000
     remainder=n-(thousands*1000)
-    words=n2w(thousands)+' thousand'
+    words=n2w(thousands)+' mil '
     if remainder>0:
       words+=' '+n2w(remainder)
     return words
   elif n<1000000000:
     millions=(n-n%1000000)/1000000
     remainder=n-(millions*1000000)
-    words=n2w(millions)+' million'
+    if millions ==1:
+      words= 'un millon '
+    else:
+      words=n2w(millions)+' millones'
     if remainder>0:
       words+=' '+n2w(remainder)
     return words
   else:
-    return 'Number out of range'
+    return 'Numero fuera de rango'
 
 def d2w(n):
+  if n == 1:
+    return 'primero'
+  else:
+    return n2w(n)
+    
+def n2m(n):
   try:
-    return date2words[n]
+    return date2month[n-1]
   except KeyError:
-    try:
-      return num2words[n-n%10] + date2words[n%10]
-    except KeyError:
-      return 'Date out of range'
+    return 'mes fuera de rango'
+    
+def n2d(n):
+  try:
+    return num2day[n]
+  except KeyError:
+    return 'dia fuera de rango'
 
