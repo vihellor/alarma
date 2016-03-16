98ecb061f80400a69b59361c6f8ed45d5cfd40b6
vihellor <vihellor@gmail.com>
2016-03-16 17:11:54 -0600

    Update better_spoken_numbers.py

--- better_spoken_numbers.py
+++ better_spoken_numbers.py
@@ -27,50 +27,13 @@ num2day = {0: 'Domingo ', 1: 'Lunes ', 2: 'Martes ', 3: 'Miercoles ', 4: 'Jueves
 
 
 def n2w(n):
-  if n<=20:
-    return num2words[n]
-  elif n<100:
-    words=num2words[n-n%10]
-    if n%10>0:
-      words+= 'y ' +num2words[n%10]
-    return words
-  elif n==100:
-    word= 'cien '
-  elif n<1000:
-    hundreds=(n-n%100)/100
-    tens=(n%100)-(n%100)%10
-    singles=n-((hundreds*100)+tens)
-    words=num2cient[hundreds]
-    if tens > 0:
-      words+=num2words[tens] + 'y '
-    if singles > 0:
-      words+=num2words[singles]
-    return words
-  elif n<1000000:
-    thousands=(n-n%1000)/1000
-    remainder=n-(thousands*1000)
-    words=n2w(thousands)+' mil '
-    if remainder>0:
-      words+=' '+n2w(remainder)
-    return words
-  elif n<1000000000:
-    millions=(n-n%1000000)/1000000
-    remainder=n-(millions*1000000)
-    if millions ==1:
-      words= 'un millon '
-    else:
-      words=n2w(millions)+' millones'
-    if remainder>0:
-      words+=' '+n2w(remainder)
-    return words
-  else:
-    return 'Numero fuera de rango'
+  return String(n)
 
 def d2w(n):
   if n == 1:
     return 'primero'
   else:
-    return n2w(n)
+    return String(n)
     
 def n2m(n):
   try:
